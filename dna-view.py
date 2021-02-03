66accb06b9f44ed16dc4280127ffba7f4fc533a8
alihsaas <alihsaas2@gmail.com>
2021-02-03 16:31:51 +0200

    utilize multiple images

--- dna-view.py
+++ dna-view.py
@@ -1,5 +1,6 @@
 from logging import error
-from PIL import Image, ImageDraw
+from PIL import Image
+from base_draw import BaseDraw
 import sys
 
 args = sys.argv
@@ -11,55 +12,41 @@ nito_bases = {
     "G": "C"
 }
 
-bases_color = {
-    "A": "red",
-    "T": "yellow",
-    "C": "blue",
-    "G": "purple"
-}
-
 bases = args[1].upper()
 
 height = len(bases) * 50 + 100
 width = 300
 
 center = width/2
-center_offset = 20
 
 current_index = 1
 
-rectangle_size = (60, 20)
+main_image = Image.new("RGB", (width, height), color="white")
 
-image = Image.new("RGB", (width, height), color="white")
-draw = ImageDraw.Draw(image)
+first_strand = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+second_strand = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+
+first_draw_strand = BaseDraw(first_strand)
+second_draw_strand = BaseDraw(second_strand)
 
 for base in bases:
+
     if base not in nito_bases:
         error("Enter a valid base sequence " + " ".join(nito_bases.keys()))
 
     current_position = current_index * 50
 
-    draw.rectangle(
-        (
+    first_draw_strand.draw_base(base, current_position)
 
-            (center - rectangle_size[0] - center_offset, current_position),
-            (center - center_offset, current_position + rectangle_size[1])
-        ),
-        fill="black"
-    )
+    comp_base = nito_bases[base]
 
-    # OTHER STAND
-
-    draw.rectangle(
-        (
-
-            (center + rectangle_size[0] + center_offset, current_position),
-            (center + center_offset, current_position + rectangle_size[1])
-        ),
-        fill="black"
-    )
+    second_draw_strand.draw_base(comp_base, current_position)
 
     current_index += 1
 
-image.save("dna-view.png")
+second_strand = second_strand.transpose(Image.FLIP_LEFT_RIGHT)
 
+main_image.paste(first_strand, (0, 0), first_strand)
+main_image.paste(second_strand, (0, 0), second_strand)
+
+main_image.save("dna-view.png")
